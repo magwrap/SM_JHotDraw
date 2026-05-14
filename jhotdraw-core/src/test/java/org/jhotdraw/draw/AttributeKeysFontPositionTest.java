aac2dccffb197fd13c7fa73748baea22d8f50155
KonyakB <bencekanyok@gmail.com>
2026-05-14 11:42:05 +0200

    Extracts StubFigure from inner class to top-level for reuse

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/AttributeKeysFontPositionTest.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/AttributeKeysFontPositionTest.java
@@ -6,13 +6,6 @@
 package org.jhotdraw.draw;
 
 import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import java.util.HashMap;
-import java.util.Map;
-import org.jhotdraw.draw.figure.AbstractFigure;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -127,74 +120,5 @@ public class AttributeKeysFontPositionTest {
         assertFalse(AttributeKeys.FONT_SUBSCRIPT.getDefaultValue());
     }
 
-    private static class StubFigure extends AbstractFigure {
 
-        private final Map<AttributeKey<?>, Object> attributes = new HashMap<>();
-
-        @Override
-        public <T> T get(AttributeKey<T> key) {
-            if (attributes.containsKey(key)) {
-                @SuppressWarnings("unchecked")
-                T value = (T) attributes.get(key);
-                return value;
-            }
-            return key.getDefaultValue();
-        }
-
-        @Override
-        public <T> void set(AttributeKey<T> key, T value) {
-            attributes.put(key, value);
-        }
-
-        @Override
-        public void draw(Graphics2D g) {
-        }
-
-        @Override
-        public Rectangle2D.Double getBounds() {
-            return null;
-        }
-
-        @Override
-        public Rectangle2D.Double getDrawingArea() {
-            return null;
-        }
-
-        @Override
-        public boolean contains(Point2D.Double p) {
-            return false;
-        }
-
-        @Override
-        public Object getTransformRestoreData() {
-            return null;
-        }
-
-        @Override
-        public void restoreTransformTo(Object restoreData) {
-        }
-
-        @Override
-        public void transform(AffineTransform tx) {
-        }
-
-        @Override
-        public Map<AttributeKey<?>, Object> getAttributes() {
-            return null;
-        }
-
-        @Override
-        public Object getAttributesRestoreData() {
-            return null;
-        }
-
-        @Override
-        public void restoreAttributesTo(Object restoreData) {
-        }
-
-        @Override
-        public Rectangle2D.Double getDrawingArea(double factor) {
-            return null;
-        }
-    }
 }
