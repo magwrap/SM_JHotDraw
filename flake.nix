f4e248e4045fbdb99cec981643aa02f07dea58ae
0rangeBlaze <gardevdaniel@gmail.com>
2026-05-18 10:10:12 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
@@ -6,19 +5,58 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
+      jdk = pkgs.openjdk17;
     in {
+      packages.default = pkgs.maven.buildMavenPackage {
+        pname = "jhotdraw";
+        version = "9.1-SNAPSHOT";
+        src = ./.;
+
+        mvnJdk = jdk;
+
+        mvnHash = "sha256-fKteUJzzFsIFWSAQYLaiuPItH+FYMJsYViMVTfcwt+4=";
+
+        nativeBuildInputs = with pkgs; [ makeWrapper ];
+
+        mvnParameters = "-DskipTests";
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/share/java $out/bin
+          find . -path '*/target/*.jar' -not -name 'original-*' -exec cp {} $out/share/java/ \;
+          classpath=$(find $out/share/java -name '*.jar' | sort | tr '\n' ':')
+          makeWrapper ${jdk}/bin/java $out/bin/jhotdraw \
+            --add-flags "-cp" \
+            --add-flags "$classpath" \
+            --add-flags "org.jhotdraw.samples.svg.Main"
+          runHook postInstall
+        '';
+
+        meta.mainProgram = "jhotdraw";
+      };
+
+      apps.svg = flake-utils.lib.mkApp {
+        drv = pkgs.writeShellScriptBin "jhotdraw-svg" ''
+          classpath=$(find "${self.packages.${system}.default}/share/java" -name '*.jar' | sort | tr '\n' ':')
+          exec ${jdk}/bin/java -cp "$classpath" org.jhotdraw.samples.svg.Main "$@"
+        '';
+      };
+      apps.draw = flake-utils.lib.mkApp {
+        drv = pkgs.writeShellScriptBin "jhotdraw-draw" ''
+          classpath=$(find "${self.packages.${system}.default}/share/java" -name '*.jar' | sort | tr '\n' ':')
+          exec ${jdk}/bin/java -cp "$classpath" org.jhotdraw.samples.draw.Main "$@"
+        '';
+      };
+
       devShells.default = pkgs.mkShell {
-        # Show a friendly prompt
         name = "java-dev-shell";
-
         packages = with pkgs; [
-          openjdk17            
-          maven                
-          gradle              
-          coursier           
+          jdk
+          maven
+          gradle
+          coursier
         ];
-
-        JAVA_HOME = "${pkgs.openjdk17}";
+        JAVA_HOME = "${jdk}";
       };
     });
 }
